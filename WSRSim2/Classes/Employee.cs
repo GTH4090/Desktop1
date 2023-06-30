7edd3ddd5495897f9fbe2c6fce990770012cf97a
ewbeb <karaevsv@sch1002.ru>
2023-06-30 13:40:01 +0300

    123

---
+++ WSRSim2/Classes/Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSRSim2.Models
+{
+    public partial class Employee
+    {
+        public int ClosedPerMonth { get
+            {
+                DateTime StartDate = DateTime.Now.Date;
+                DateTime EndDate = DateTime.Now.Date;
+                StartDate = new DateTime(StartDate.Year, StartDate.Month, 1);
+                EndDate = StartDate.AddMonths(1).AddDays(-1);
+                DateTime start = new DateTime();
+                DateTime end = new DateTime();
+                int res = 0;
+                foreach(var  item in this.Task)
+                {
+                    if (item.StartActualTime == null)
+                    {
+                        start = (DateTime)item.CreatedTime;
+                    }
+                    else
+                    {
+                        start = (DateTime)item.StartActualTime;
+                    }
+                    if (item.FinishActualTime == null)
+                    {
+                        end = (DateTime)item.Deadline;
+                    }
+                    else
+                    {
+                        end = (DateTime)item.FinishActualTime;
+                    }
+                    if (((start.Date < StartDate && end.Date >= StartDate.Date) || (start.Date >= StartDate)) && item.StatusId == 3)
+                    {
+                        res++;
+                    }
+                }
+                return res;
+                
+            } }
+        public int DeadlinedPerMonth
+        {
+            get
+            {
+                DateTime StartDate = DateTime.Now.Date;
+                DateTime EndDate = DateTime.Now.Date;
+                StartDate = new DateTime(StartDate.Year, StartDate.Month, 1);
+                EndDate = StartDate.AddMonths(1).AddDays(-1);
+                DateTime start = new DateTime();
+                DateTime end = new DateTime();
+                int res = 0;
+                foreach (var item in this.Task)
+                {
+                    if (item.StartActualTime == null)
+                    {
+                        start = (DateTime)item.CreatedTime;
+                    }
+                    else
+                    {
+                        start = (DateTime)item.StartActualTime;
+                    }
+                    if (item.FinishActualTime == null)
+                    {
+                        end = (DateTime)item.Deadline;
+                    }
+                    else
+                    {
+                        end = (DateTime)item.FinishActualTime;
+                    }
+                    if (((start.Date < StartDate && end.Date >= StartDate.Date) || (start.Date >= StartDate)) && item.Deadline < DateTime.Now)
+                    {
+                        res++;
+                    }
+                }
+                return res;
+
+            }
+        }
+    }
+}
--- WSRSim2/Pages/DashBoard.xaml.cs
+++ WSRSim2/Pages/DashBoard.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static WSRSim2.Classes.Helper;
+using WSRSim2.Models;
+using System.Windows.Threading;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace WSRSim2.Pages
 {
@@ -21,10 +26,102 @@ namespace WSRSim2.Pages
     /// </summary>
     public partial class DashBoard : Page
     {
+        DispatcherTimer Timer = new DispatcherTimer();
         public DashBoard()
         {
             InitializeComponent();
             File.WriteAllText("Memory.txt", "1");
+            Timer.Interval = new TimeSpan(0, 0, 30);
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            loadData();
+        }
+        private void CheckSize()
+        {
+            if(MainGrid.ActualWidth < 1440)
+            {
+                Grid.SetRow(OpenPerWeedSp, 1);
+                Grid.SetColumn(OpenPerWeedSp, 0);
+                Grid.SetColumn(Top5BestSp, 1);
+                Grid.SetColumn(Top5WorstSp, 2);
+            }
+            else
+            {
+                Grid.SetRow(OpenPerWeedSp, 0);
+                Grid.SetColumn(OpenPerWeedSp, 3);
+                Grid.SetColumn(Top5BestSp, 0);
+                Grid.SetColumn(Top5WorstSp, 1);
+            }
+        }
+
+        private void loadData()
+        {
+
+            try
+            {
+                List<Models.Task> openedTasks = Db.Task.Where(el => el.ProjectId == SelectedProject.Id && el.StatusId == 1).ToList();
+                openedtaskDataGrid.ItemsSource = openedTasks;
+                OpenedCountLbl.Content = openedTasks.Count();
+
+                List<Models.Task> deadlinedTasks = Db.Task.Where(el => el.ProjectId == SelectedProject.Id && el.Deadline < DateTime.Now).ToList();
+                if(deadlinedTasks.Count() > 2)
+                {
+                    DeadlinedSp.Background = Brushes.Red;
+                }
+                deadlinedtaskDataGrid.ItemsSource = deadlinedTasks.ToList();
+                DeadlinedCountLbl.Content = deadlinedTasks.Count();
+
+                List<Models.Task> activeTasks = Db.Task.Where(el => el.ProjectId == SelectedProject.Id && el.StatusId == 2 && (el.StartActualTime <= DateTime.Now || el.CreatedTime <= DateTime.Now) &&
+                (el.FinishActualTime >= DateTime.Now || el.Deadline >= DateTime.Now)).ToList();
+                ActiveCountLbl.Content = activeTasks.Count();
+                activetaskDataGrid.ItemsSource = activeTasks;
+                if(activeTasks.Count() == 0 && DateTime.Now.Hour >= 9 && DateTime.Now.Hour <= 18)
+                {
+                    ActiveSp.Background = Brushes.Red;
+                }
+
+                DateTime weekstart = DateTime.Now.Date;
+                DateTime weekend = DateTime.Now.Date;
+                int weekDay = (int)weekend.DayOfWeek;
+                if (weekDay != 0)
+                {
+                    weekend = weekend.AddDays(7 - weekDay);
+                }
+                weekstart = weekend.AddDays(-6);
+                List<Models.Task> openedperWeekTasks = Db.Task.Where(el => el.ProjectId == SelectedProject.Id && el.StatusId == 1 && ((el.StartActualTime <= weekstart || el.CreatedTime <= weekstart)
+                && (el.FinishActualTime >= weekstart || el.Deadline >= weekstart) || (el.StartActualTime >= weekstart || el.CreatedTime >= weekstart)
+                && (el.StartActualTime <= weekend || el.CreatedTime <= weekend))).ToList();
+                OpenPerWeekCountLbl.Content = openedperWeekTasks.Count();
+                OpenPerWeekDataGrid.ItemsSource = openedperWeekTasks;
+
+                List<Employee> bestEmployee = Db.Employee.Where(el => el.Task.FirstOrDefault(e => e.ProjectId == SelectedProject.Id) != null
+                ).ToList().OrderByDescending(el => el.ClosedPerMonth).Take(5).ToList();
+                BestemployeeDataGrid.ItemsSource = bestEmployee;
+
+                List<Employee> worstEmployee = Db.Employee.Where(el => el.Task.FirstOrDefault(e => e.ProjectId == SelectedProject.Id) != null
+                ).ToList().OrderByDescending(el => el.DeadlinedPerMonth).Take(5).ToList();
+                WorstemployeeDataGrid.ItemsSource = worstEmployee;
+
+            }
+            catch (Exception ex)
+            {
+                Error(ex.Message);
+            }
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CheckSize();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            loadData();
+            CheckSize();
         }
     }
 }
